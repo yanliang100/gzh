34316ee114e0ec9c203b45df27784683a9cde1bf
liang.yan <yanliang100@sina.cn>
2019-05-16 11:42:17 +0800

    微信公众号初始化

---
+++ src/test/java/com/lafei/gzh/GzhApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lafei.gzh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GzhApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
+
